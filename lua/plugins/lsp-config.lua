5b5268a8e4916703ce526be4bf07eddeaaf3a1fa
Nathan Osti <nathanosti@192.168.0.10>
2024-02-15 13:41:49 -0300

    Add waka-time to monitor my productivity

--- lua/plugins/lsp-config.lua
+++ lua/plugins/lsp-config.lua
@@ -35,22 +35,16 @@ return {
 			keymap.set("n", "<leader>td", vim.lsp.buf.type_definition, opts) -- show buffer type definitions
 
 			opts.desc = "See available code actions"
-			keymap.set({ "n", "v" }, "<c-ca>", vim.lsp.buf.code_action, opts) -- see available code actions, in visual mode will apply to selection
+			keymap.set({ "n", "v" }, "<c-ca>", vim.lspnbuf.code_action, opts) -- see available code actions, in visual mode will apply to selection
 
 			opts.desc = "Smart rename"
 			keymap.set("n", "<leader>rn", vim.lsp.buf.rename, opts) -- smart rename
 
 			opts.desc = "Show buffer diagnostics"
-			keymap.set("n", "<leader>D", "<cmd>Telescope diagnostics bufnr=0<CR>", opts) -- show  diagnostics for file
+			keymap.set("n", "]d", "<cmd>Telescope diagnostics bufnr=0<CR>", opts) -- show  diagnostics for file
 
 			opts.desc = "Show line diagnostics"
-			keymap.set("n", "<leader>d", vim.diagnostic.open_float, opts) -- show diagnostics for line
-
-			opts.desc = "Go to previous diagnostic"
-			keymap.set("n", "[d", vim.diagnostic.goto_prev, opts) -- jump to previous diagnostic in buffer
-
-			opts.desc = "Go to next diagnostic"
-			keymap.set("n", "]d", vim.diagnostic.goto_next, opts) -- jump to next diagnostic in buffer
+			keymap.set("n", "[d", vim.diagnostic.open_float, opts) -- show diagnostics for line
 
 			opts.desc = "Show documentation for what is under cursor"
 			keymap.set("n", "K", "<cmd>lua vim.lsp.buf.hover()<CR>", opts) -- show documentation for what is under cursor
